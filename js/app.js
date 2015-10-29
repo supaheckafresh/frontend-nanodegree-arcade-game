6d8a0bb2289a46c2ee448b8928c1bb5766181f19
supaheckafresh <neilstrain@gmail.com>
2015-10-28 22:21:26 -0700

    Move this.crawl() into update prototype. Also multiply movement by dt.

--- js/app.js
+++ js/app.js
@@ -14,18 +14,18 @@ var Enemy = function () {
     this.speed = Math.floor(Math.random() * 5) + 1;
 };
 
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function () {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    this.crawl();
+    this.crawl() * dt;
 };
 
 Enemy.prototype.crawl = function () {
